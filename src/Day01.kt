63ff205049eb1ade37db45573e6eef2cf32bbc1e
Andrew Gadion <andrew.gadion@gmail.com>
2023-12-01 23:10:10 +0200

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
+import java.lang.Exception
+
+enum class Digits {
+    zero, one, two, three, four, five, six, seven, eight, nine;
+    override fun toString() = ordinal.toString()
+    companion object {
+        fun fromString(str: String) =
+            Digits.entries.find { it.name == str || it.ordinal.toString() == str } ?: throw Exception()
+    }
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getPart1Number(str: String) = "${str.find(Char::isDigit)}${str.findLast(Char::isDigit)}".toInt()
+
+    fun Regex.findLast(str: String) = str.indices.reversed().firstNotNullOfOrNull { this.find(str, it) }
+
+    val digitRegex = Regex(Digits.entries.joinToString("|") { "${it.name}|${it.ordinal}" })
+    fun getPart2Number(str: String): Int {
+        val digits = listOfNotNull(digitRegex.find(str)?.value, digitRegex.findLast(str)?.value)
+            .map { Digits.fromString(it) }
+
+        return "${digits.first()}${digits.last()}".toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input.sumOf(::getPart1Number)
+    fun part2(input: List<String>) = input.sumOf(::getPart2Number)
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("day1")
     part1(input).println()
     part2(input).println()
 }
